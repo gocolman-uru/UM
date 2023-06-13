f456612cdbd8aa70ddedf4d2f9a0b4f583f0a5ef
Gonzalo Colman <gonzalocol96@gmail.com>
2023-06-12 21:23:32 -0300

    buscador listo

--- test/app.py
+++ test/app.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager, login_user, logout_user, login_required, U
 import json
 import requests
 from flask_wtf.csrf import CSRFProtect
-
+import csv
 
 
 
@@ -20,25 +20,15 @@ login_manager.init_app(app)
 
 
 
-# Directorio de la base de datos
-#dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
-
-
-
-# Configuración de la base de datos de usuarios (puede ser cualquier base de datos compatible con Flask)
 class User(UserMixin):
     def __init__(self, id):
         self.id = id
 
-users = {'admin': {'password': 'admin123'}, 'user': {'password': 'user123'}}
+users = {'admin': {'password': 'admin123'}, 'gcolman1': {'password': 'gcolman1'}}
 
 @login_manager.user_loader
 def load_user(user_id):
     return User(user_id)
-
-
-### Genero las clases necesarias que necesito
-
 
 
 # Rutas de la aplicación
@@ -81,9 +71,18 @@ def logout():
 
 @app.route('/search')
 def search():
-    # tomo el valor de la busqueda
+
     query = request.args.get('query')
-    return render_template('search.html')
+    with open('1_bases_de_datos_ficticias/bases/libros.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        results_final = []
+        for row in reader:
+            if str(query).lower() in row[0].lower():
+                results_nombre = [row[0].split('|')[1]]
+                results_link = [row[0].split('|')[7]]
+                print([results_nombre+results_link])
+                results_final.append([results_nombre+results_link])
+    return render_template('search.html', results=results_final)
 
 
 
@@ -100,43 +99,3 @@ def search():
 
 
 
-
-
-
-
-@app.route('/search_results')
-def search_results():
-    query = request.args.get('query')
-    # Aquí iría tu código para buscar libros en tu base de datos o en la web
-    return render_template('search_results.html', books=books)
-
-
-
-
-
-
-
-
-'''@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username in users and password == users[username]['password']:
-            user = User(username)
-            login_user(user)
-            return redirect(url_for('home'))
-        else:
-            return render_template('login.html', error='Nombre de usuario o contraseña incorrectos')
-    else:
-        return render_template('login.html')'''
-'''
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-'''
-if __name__ == '__main__':
-    csrf.init_app(app)
-    app.run(debug=True)
